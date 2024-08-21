7fb48ce2556e82263ec7cbe40e47201fdf54de78
Zhou Zhenglong <zlzhou.sh@outlook.com>
2024-08-21 10:51:30 +0800

    Mark editor preview as Experimental

--- assets/examples/tutorial-006-builtin/builtin-dev-pipeline-settings.ts
+++ assets/examples/tutorial-006-builtin/builtin-dev-pipeline-settings.ts
@@ -78,7 +78,10 @@ export class BuiltinDevPipelineSettings extends Component {
     @property(CCBoolean)
     protected _editorPreview = false;
 
-    @property(CCBoolean)
+    @property({
+        displayName: 'Editor Preview (Experimental)',
+        type: CCBoolean,
+    })
     get editorPreview(): boolean {
         return this._editorPreview;
     }
